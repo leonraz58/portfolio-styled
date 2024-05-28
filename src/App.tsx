99478445c89ad0b26c4a7e819d815f374e12b880
leonraz58 <leonraz58@gmail.com>
2024-05-29 00:08:30 +0300

    add Testimony

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import './App.css';
 import {Header} from "./layout/header/Header";
 import {Main} from "./layout/sections/main/Main";
 import {Skills} from "./layout/sections/skills/Skills";
+import {Works} from "./layout/sections/works/Works";
+import {Testimony} from "./layout/sections/testimony/Testimony";
 
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
             <Header/>
             <Main/>
             <Skills/>
+            <Works/>
+            <Testimony/>
 
         </div>
     );
