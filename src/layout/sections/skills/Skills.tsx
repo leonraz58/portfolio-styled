1227d2be3dc9e37c93fdad887ef827b6d56d5c66
leonraz58 <leonraz58@gmail.com>
2024-07-06 21:42:18 +0300

    Skills on scroll animation (React-awesome-reveal)

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,6 +4,7 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import {Skill} from "./skill/Skill";
 import {Container} from "../../../components/Container";
 import {S} from './Skills_Styles'
+import {Fade} from "react-awesome-reveal";
 
 const skillData = [
     {
@@ -44,13 +45,15 @@ export const Skills: React.FC = () => {
             <Container>
                 <SectionTitle>My Skills</SectionTitle>
                 <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-                    {skillData.map((skill, index) => {
-                        return <Skill iconId={skill.iconId}
-                                      title={skill.title}
-                                      description={skill.description}
-                                      key={index}
-                        />
-                    })}
+                    <Fade cascade={true} damping={0.2}>
+                        {skillData.map((skill, index) => {
+                            return <Skill iconId={skill.iconId}
+                                          title={skill.title}
+                                          description={skill.description}
+                                          key={index}
+                            />
+                        })}
+                    </Fade>
                 </FlexWrapper>
             </Container>
         </S.StyledSkills>
